7073b93bd3b277f1a15033deaefffd89286af868
Timster Lipkens <tim.lipkens@gmail.com>
2025-07-09 22:38:33 +0300

    name and run

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static void name() {
+        System.out.print("May I have your name? ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
